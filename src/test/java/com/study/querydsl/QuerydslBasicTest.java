0c864058eef20b0552c9b6a5661cdcae4f33bc50
오한별 <suriessay@naver.com>
2020-11-07 23:59:21 +0900

    dsl paging test

--- src/test/java/com/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/com/study/querydsl/QuerydslBasicTest.java
@@ -150,4 +150,16 @@ public class QuerydslBasicTest {
         assertThat(member2.getUsername()).isEqualTo("member5");
         assertThat(memberNull.getUsername()).isNull();
     }
+
+    @Test
+    public void paging(){
+        List<Member> result = queryFactory
+                .selectFrom(member)
+                .orderBy(member.username.desc())
+                .offset(1)
+                .limit(2)
+                .fetch();
+
+        assertThat(result.size()).isEqualTo(2);
+    }
 }
